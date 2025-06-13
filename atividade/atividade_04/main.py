d11507515a9d1c29fc6f5f5d835fc627ca292d4c
matheusribeirorocha <matheusrri16@gmail.com>
2025-06-13 17:29:17 -0300

    ok

--- atividade/atividade_04/main.py
+++ atividade/atividade_04/main.py
@@ -9,7 +9,9 @@ while True:
     print("1 - Inserir novo nome na lista")
     print("2 - Exibir lista")
     print("3 - Pesquisar nome na lista")
-    print("4 - Sair do programa")
+    print("4 - Alterar nome na lista")
+    print("5 - Excluir nome na lista")
+    print("6 - Sair do programa")
 
     opcao = input("Opção desejada: ").strip()
 
@@ -28,8 +30,8 @@ while True:
         case "2":
             print("\nLista:\n")
             try:
-                for nome in nomes: # type: ignore
-                    print(nome) # type: ignore
+                for i in range(len(nomes)): # type: ignore
+                    print(f"Índice {i}: {nomes[i]}.")
                 print("\n")
             except Exception as e:
                 print(f"Não foi possível exibir a lista. {e}.")
@@ -42,7 +44,31 @@ while True:
             print(f"{nome_pesquisado} foi encontrado {result} vezes.")
             continue
         case "4":
-            print("Programa encerrado! ;) ")
+            try:
+                i = int(input("Informe o índice que deseja alterar: "))
+                if i >= 0 and i < len(nomes): # type: ignore
+                    nomes[i] = input("Informe o novo nome: ")
+                    print("Nome alterado com sucesso.")
+                else:
+                    print("Índice inválido.")
+            except Exception as e:
+                print(f"Não foi possível alterar item da lista. {e}.")
+            finally:
+                continue
+        case "5":
+            try:
+                i = int(input("Informe o índice que deseja excluir: "))
+                if i >= 0 and i < len(nomes): # type: ignore
+                    del(nomes[i])
+                    print("Nome excluído com sucesso.")
+                else:
+                    print("Índice inválido.")
+            except Exception as e:
+                print(f"Não foi possível excluir nome. {e}.")
+            finally:
+                continue
+        case "6":
+            print("Programa encerrado! =D ")
             break
         case _:
             print("Opção inválida.")
